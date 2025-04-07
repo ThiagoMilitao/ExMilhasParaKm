62980b8cfc8ee3f91294b1f51f17875e5c545c14
thiagoMilitao <thiago.militao2007@gmail.com>
2025-04-07 18:13:23 -0300

    milhasParaKm

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite a distância em milhas:");
+double milhas = double.Parse(Console.ReadLine()!);
+
+double km = milhas * 1.609;
+Console.WriteLine($"A distância em quilômetros é: {km} km");    
